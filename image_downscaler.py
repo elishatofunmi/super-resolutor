1df8d45f1582a90bda1bb561cab053aa1abe6a97
elishatofunmi <elishatofunmi@gmail.com>
2020-04-08 21:12:34 -0400

    commit

--- image_downscaler.py
+++ image_downscaler.py
@@ -3,25 +3,35 @@ import cv2
 
 
 class DownScaler:
-    def __init__(self, k, image, output_dir = None):
+    def __init__(self, image, output_dir = None):
         """
         k: is the scaling factor (an integer) not a float
         image: is the directory of image to be rescaled
         output_dir: This is the directory to which the downscaled data is saved to
         """
-        self.k = k
+        
         self.img = image
         self.output_dir = output_dir
         return
 
-    def rescale(self):
-        img = cv2.imread(self.img)
-        x,y,z = img.shape
-        new_scale = int(x/k), int(y/k), int(z/k)
-
-        img_resized = img.resize(new_scale)
-        # get image name from directory of the image to be transformed
-        
-        image_name = self.output_dir + image[]
-        cv2.imwrite(img_resized, image_name)
+    
+    def rescale(self, image):
+ 
+        img = cv2.imread(image, cv2.IMREAD_UNCHANGED)
+ 
+        dim = img.shape
+ 
+        width = int(dim[0]/2)
+        height = int(dim[1]/2) # keep original height
+        dim = (width, height)
+ 
+        # resize image
+        resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+        file_name = image.split('.')[-2].spilt('/')[-1]
+        image_dir = 
+        if output_dir == None:
+            name =  + '/' + file_name
+        cv2.imwrite(name, resized)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows() 
         return
